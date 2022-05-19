a96c082522051635976457e9c177860b09165a93
Mike Vardy <mike.vardy@redspace.com>
2022-05-19 04:44:31 -0400

    tabbed content example

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import { createSignal, onCleanup } from "solid-js";
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createSignal, Suspense, Switch, Match, useTransition } from "solid-js";
+import Content from "./elements/Content";
 
-function App() {
-  const [count, setCount] = createSignal(0);
-	const interval = setInterval(
-		() => setCount(c => c + 1),
-		1000
-	);
-	onCleanup(() => clearInterval(interval));
+const App = () => {
+  const [tab, setTab] = createSignal(0);
+  const [pending, start] = useTransition();
+  const updateTab = (index) => () => start(() => setTab(index));
 
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Count value is: {count()}
-        </p>
-      </header>
-    </div>
+    <>
+      <ul class="inline">
+        <li classList={{ selected: tab() === 0 }} onClick={updateTab(0)}>
+          One
+        </li>
+        <li classList={{ selected: tab() === 1 }} onClick={updateTab(1)}>
+          Two
+        </li>
+        <li classList={{ selected: tab() === 2 }} onClick={updateTab(2)}>
+          Three
+        </li>
+      </ul>
+      <div class="tab" classList={{ pending: pending() }}>
+        <Suspense fallback={<div class="loader">Loading...</div>}>
+          <Switch>
+            <Match when={tab() === 0}>
+              <Content page="One" />
+            </Match>
+            <Match when={tab() === 1}>
+              <Content page="Two" />
+            </Match>
+            <Match when={tab() === 2}>
+              <Content page="Three" />
+            </Match>
+          </Switch>
+        </Suspense>
+      </div>
+    </>
   );
 }
 
---
+++ src/elements/Content.jsx
@@ -0,0 +1,27 @@
+import { createResource } from "solid-js";
+
+const CONTENT = {
+    One: `Day sales outstanding estimated time of arrival CVENT prospect market segmentation cost per lead loss aversion kickers spam score sales sequence load in sound bite martech stack monthly recurring revenue. Domain authority cancellation clause market segmentation meetings, expositions, events, and convention end of quarter ABM display & programmatic advertising conversion rate lead management draw on sales commission bounce rate content marketing. Social, military, educational, religious, and fraternal proof of concept business to consumer conversion path IPAAS cloud/data integration & tag management estimated departure time public address system always be closing lifetime value to customer acquisition cost positioning statement buying criteria complex sale DMP. Dynamic ad page authority impressions omnichannel events meetings & webinars wrap-up call analytics & management asset customer lifetime value projects & workflow Zoom customer relationship management.`,
+    Two: `Adobe request for information meetings, expositions, events, and convention call analytics & management drayage native/content advertising customer relationship management purchase stage closed-won social media marketing & monitoring complex sale. Traffic flow content management system workflow projects & workflow day sales outstanding campaign metrics estimated time of arrival needs assessment get-in contingency plan end of month customer relationship management prospect inbound marketing. Adobe live chat & chatbots colloquium gatekeeper space only influencers omnichannel content management system budgeting & finance org structure drip marketing campaigns. BrightCove quota advocacy loyalty & referrals hybrid event lead capture form customer relationship management audience segmentation vendor analysis weighted pipeline certified meeting professional.`,
+    Three: `Responsive design business to business display & programmatic advertising audience segmentation application programming interface request for tender honorarium impressions challenger sales model cross-selling. Request for proposal no-show ABM software as a service social, military, educational, religious, and fraternal audience segmentation baseline internet of things marketing sales funnel marketing funnel. Closed-lost load in specifications sales cycle Sitecore breakdown / load out / strike hosted dialer case study marketing funnel martech stack net new business display & programmatic advertising Hootsuite. Live chat & chatbots video advertising account executive responsive design lead segmentation pay-per-click asset sales cycle needs assessment end of month data protection.`
+};
+
+function createDelay() {
+    return new Promise((resolve) => {
+        const delay = Math.random() * 420 + 160;
+        setTimeout(() => resolve(delay), delay);
+    });
+}
+
+const Content = (props) => {
+    const [time] = createResource(createDelay);
+
+    return (
+        <div class="tab-content">
+            This content is for page "{props.page}" after {time()?.toFixed()}ms.
+            <p>{CONTENT[props.page]}</p>
+        </div>
+    );
+};
+
+export default Content;
